8a950f43e574f6c61ba0e0e9568ef3345f16a1e6
marcus bianchi <marcusbianchi92@hotmail.com>
2021-05-31 16:10:36 -0300

    V.3

--- EXERCISE 01 - COMPARISON LINQ LAMBDA/Program.cs
+++ EXERCISE 01 - COMPARISON LINQ LAMBDA/Program.cs
@@ -10,7 +10,9 @@ namespace EXERCISE_01___COMPARISON_LINQ_LAMBDA {
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            list.Sort(CompareProducts);
+            Comparison<Product> comp = CompareProducts;
+
+            list.Sort(comp);
 
             foreach(Product p in list) {
                 Console.WriteLine(p);
