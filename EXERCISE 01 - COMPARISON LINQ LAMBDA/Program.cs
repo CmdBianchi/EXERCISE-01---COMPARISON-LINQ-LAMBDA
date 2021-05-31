68cbbc4b3560938db204611ba40e8530f3048ab1
marcus bianchi <marcusbianchi92@hotmail.com>
2021-05-31 16:15:57 -0300

    V.5 WITH LAMBDA

--- EXERCISE 01 - COMPARISON LINQ LAMBDA/Program.cs
+++ EXERCISE 01 - COMPARISON LINQ LAMBDA/Program.cs
@@ -10,9 +10,7 @@ namespace EXERCISE_01___COMPARISON_LINQ_LAMBDA {
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-
-            list.Sort(comp);
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
             foreach(Product p in list) {
                 Console.WriteLine(p);
